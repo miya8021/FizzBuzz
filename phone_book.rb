656d12f5cfdcdafd01fa91e7dc08e0eb7cfafc83
miya <rinn.sasami@gmail.com>
2021-03-20 09:52:41 +0900

    電話帳問題

--- phone_book.rb
+++ phone_book.rb
@@ -1,4 +1,5 @@
 class NameIndex
+  #ハッシュにキーとバリューを設定。%W[配列]
   NAME_INDEX_HASH = {
     'ア' => %w[ア イ ウ エ オ ヴ],
     'カ' => %w[カ キ ク ケ コ ガ ギ グ ゲ ゴ],
@@ -10,6 +11,17 @@ class NameIndex
     'ヤ' => %w[ヤ ユ ヨ],
     'ラ' => %w[ラ リ ル レ ロ],
     'ワ' => %w[ワ ヲ ン]
-  }.freeze
+  }.freeze #オブジェクトの変更を禁止
 
+  def self.create_index(names)
+    #sort並び替えし、group_byでキーと対応する要素の配列を値とするハッシュを返す
+    names.sort.group_by {|name|
+      NAME_INDEX_HASH.find(['Others']) {|_, value| value.include?(name[0])}.first
+    }.to_a
+  end
 end
+
+
+names = ['キシモト', 'イトウ', 'ババ', 'カネダ', 'ワダ', 'ハマダ']
+
+p NameIndex.create_index(names)
